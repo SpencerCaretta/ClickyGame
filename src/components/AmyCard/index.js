900208e04088435b27cfaa24c664e2a0fb9eb958
Spencer Caretta <spencer.caretta@gmail.com>
2019-02-12 17:00:22 -0600

    added a bunch of card components

---
+++ src/components/AmyCard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function AmyCard() {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img
+          alt="SpongeBob"
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Spongebob-squarepants.svg/666px-Spongebob-squarepants.svg.png"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AmyCard;
